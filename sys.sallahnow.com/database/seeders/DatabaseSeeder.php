f9cdd41f68b970117125ddd4779bbd4f22b5cb5e
Yottaline <yottaline@gmail.com>
2024-02-11 11:31:43 +0200

    New updates in interface and solve some issues

--- sys.sallahnow.com/database/seeders/DatabaseSeeder.php
+++ sys.sallahnow.com/database/seeders/DatabaseSeeder.php
@@ -18,15 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('1234567'),
-            'mobile' => '0122356718',
-            'group' => 1,
-        ]);
-        $this->
-        call(PermissionsSeeder::class);
-
+        // \App\Models\User::create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password' => Hash::make('1234567'),
+        //     'mobile' => '0122356718',
+        //     'group' => 1,
+        // ]);
+        $this->call(PermissionsSeeder::class);
     }
 }
