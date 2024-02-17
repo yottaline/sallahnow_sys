c41981ef915360b44767e38f5460412563959ce2
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-02-17 12:55:19 +0200

    edits friday tusk

---
+++ sys.sallahnow.com/app/Http/Controllers/CenterController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Center;
+use App\Models\Technician;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class CenterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index() {
+        return view('content.centers.index');
+    }
+
+    public function load() {
+        $centers = Center::orderBy('created_at', 'desc')->limit(15)->get();
+        echo json_encode($centers);
+    }
+
+    public function submit(Request $request) {
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        $logo = $request->file('logo');
+        $logoName = $logo->hashName();
+        $location = 'Image/Center/';
+        $logo->move($location , $logoName);
+
+        $logoPath = url('Image/Center', $logoName);
+
+        $parm = [
+            'name'       => $request->name,
+            'mobile'     => $request->mobile,
+            'whatsapp'   => $request->whatsapp,
+            'email'      => $request->email,
+            'tel'        => $request->tel,
+            'logo'       => $logoPath,
+            'tax_number' => $request->tax_number,
+            'cr_number'  => $request->cr_number,
+            'country_id' => $request->country_id,
+            'state_id'   => $request->state_id,
+            'city_id'    => $request->city_id,
+            'area_id'    => $request->area_id,
+            'address'    => $request->address,
+            'owner'      => 1
+            ];
+
+
+
+        $id = $request->center_id;
+        if(!$id) {
+            $status = Center::create($parm);
+            $id = $status->id;
+        }else {
+            $status = Center::where('id', $id)->update($parm);
+        }
+
+        $record =  Center::where('id', $id)->first();
+        echo json_encode([
+            'status' => boolval($status),
+            'data' => $record,
+        ]);
+    }
+
+    public function getTechnician($item) {
+        $technician_name = Technician::where('code', 'like', '%' . $item . '%')->get();
+        echo json_encode($technician_name);
+    }
+
+    public function addOwner(Request $request){
+        $id = $request->center_id;
+       $status = Center::where('id', $id)->update([
+            'owner'  => $request->technician_name,
+        ]);
+        echo json_encode([
+            'status' => boolval($status),
+        ]);
+    }
+
+    public function getTechnicianName() {
+        $technician_name = DB::table('technicians')
+        ->join('centers', 'technicians.id', '=', 'centers.owner')
+        ->select('centers.name','technicians.name')->orderBy('centers.created_at', 'desc')
+        ->get();
+        echo json_encode($technician_name);
+    }
+}
