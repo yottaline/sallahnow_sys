8a9c9b17e167a8bedc060ba4adaf220c99c5d649
Yottaline <yottaline@gmail.com>
2024-05-25 11:35:03 +0300

    Update db file

--- docs/db.sql
+++ docs/db.sql
@@ -726,22 +726,16 @@ CREATE TABLE IF NOT EXISTS `tech_form` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `market_retailers` (
-  `retailer_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `retailer_name` VARCHAR(100) NOT NULL,
-  `retailer_email` VARCHAR(120) NOT NULL,
-  `retailer_phone` VARCHAR(24) NOT NULL,
-  `retailer_password` VARCHAR(250) NOT NULL,
-  `retailer_store` INT UNSIGNED NOT NULL,
-  `retailer_admin` BOOLEAN NOT NULL DEFAULT '0',
-  `retailer_active` BOOLEAN NOT NULL DEFAULT '1',
---   `retailer_approved` BOOLEAN NOT NULL DEFAULT '0',
-  `retailer_approved_by` INT UNSIGNED  DEFAULT NULL,
-  `retailer_approved` DATETIME DEFAULT NULL,
-  `retailer_register` DATETIME NOT NULL,
-  PRIMARY KEY (`retailer_id`),
-  KEY (`retailer_store`) REFERENCES (`stores`),
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `market_order_items`;
+DROP TABLE IF EXISTS `market_orders`;
+DROP TABLE IF EXISTS `market_product_photos`;
+DROP TABLE IF EXISTS `market_product_views`;
+DROP TABLE IF EXISTS `market_products`;
+DROP TABLE IF EXISTS `market_retailers`;
+DROP TABLE IF EXISTS `market_stores`;
+DROP TABLE IF EXISTS `market_subcategories`;
+DROP TABLE IF EXISTS `market_categories`;
 
 -- --------------------------------------------------------
 
@@ -763,31 +757,47 @@ CREATE TABLE IF NOT EXISTS `market_stores` (
   `store_status` INT UNSIGNED DEFAULT 1,
   `store_cerated` DATETIME NOT NULL,
   PRIMARY KEY (`store_id`),
+  UNIQUE (`store_code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `market_retailers` (
+  `retailer_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `retailer_name` VARCHAR(100) NOT NULL,
+  `retailer_email` VARCHAR(120) NOT NULL,
+  `retailer_phone` VARCHAR(24) NOT NULL,
+  `retailer_password` VARCHAR(250) NOT NULL,
+  `retailer_store` INT UNSIGNED NOT NULL,
+  `retailer_admin` BOOLEAN NOT NULL DEFAULT '0',
+  `retailer_active` BOOLEAN NOT NULL DEFAULT '1',
+  `retailer_approved` BOOLEAN NOT NULL DEFAULT '0',
+  `retailer_approved_by` INT UNSIGNED  DEFAULT NULL,
+  -- `retailer_approved` DATETIME DEFAULT NULL,
+  `retailer_register` DATETIME NOT NULL,
+  PRIMARY KEY (`retailer_id`),
+  FOREIGN KEY (`retailer_store`) REFERENCES `market_stores`(`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `market_categories` (
   `category_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(1024) NOT NULL,
-  PRIMARY KEY (`category_id`),
+  PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `market_subcategories` (
   `subcategory_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `subcategory_name` VARCHAR(1024) NOT NULL,
-  `subcategory_cat` VARCHAR(1024) NOT NULL,
+  `subcategory_cat` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`subcategory_id`),
-  FOREIGN KEY (`subcategory_cat`) REFERENCES `market_categories`(`category_id`),
+  FOREIGN KEY (`subcategory_cat`) REFERENCES `market_categories`(`category_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `market_products` (
-  `product_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `product_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `product_code` VARCHAR(24) NOT NULL,
   `product_name` VARCHAR(1024) NOT NULL,
   `product_store` INT UNSIGNED NOT NULL,
@@ -795,8 +805,8 @@ CREATE TABLE IF NOT EXISTS `market_products` (
   `product_subcategory` INT UNSIGNED NOT NULL,
   `product_photo` BIGINT UNSIGNED NOT NULL,
   `product_desc` TEXT NOT NULL,
-  `product_price` DECIMAL(2, 9) NOT NULL,
-  `product_disc` DECIMAL(2, 9) NOT NULL
+  `product_price` DECIMAL(9, 2) NOT NULL,
+  `product_disc` DECIMAL(9, 2) NOT NULL,
   `product_views` INT UNSIGNED NOT NULL,
   `product_show` BOOLEAN DEFAULT '1',
   `product_delete` BOOLEAN DEFAULT '0',
@@ -805,33 +815,31 @@ CREATE TABLE IF NOT EXISTS `market_products` (
   FOREIGN KEY (`product_store`) REFERENCES `market_stores`(`store_id`),
   FOREIGN KEY (`product_category`) REFERENCES `market_categories`(`category_id`),
   FOREIGN KEY (`product_subcategory`) REFERENCES `market_subcategories`(`subcategory_id`),
+  UNIQUE (`product_code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `market_product_photos` (
   `photo_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `photo_file` VARCHAR(64) NOT NULL,
-  `photo_product` BIGINT UNSIGNED NOT NULL,
+  `photo_product` INT UNSIGNED NOT NULL,
   `photo_cerated` DATETIME NOT NULL,
   PRIMARY KEY (`photo_id`),
-  FOREIGN KEY (`photo_product`) REFERENCES `market_products`(`product_id`),
+  FOREIGN KEY (`photo_product`) REFERENCES `market_products`(`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `market_product_views` (
   `view_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `view_customer` INT UNSIGNED NOT NULL,
-  `view_product` BIGINT UNSIGNED NOT NULL,
+  `view_product` INT UNSIGNED NOT NULL,
   `view_cerated` DATETIME NOT NULL,
   PRIMARY KEY (`view_id`),
   FOREIGN KEY (`view_customer`) REFERENCES `customers`(`customer_id`),
-  FOREIGN KEY (`view_product`) REFERENCES `market_products`(`product_id`),
+  FOREIGN KEY (`view_product`) REFERENCES `market_products`(`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `market_orders` (
   `order_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `order_code` VARCHAR(12) NOT NULL,
@@ -849,10 +857,10 @@ CREATE TABLE IF NOT EXISTS `market_orders` (
   `order_delivered` DATETIME DEFAULT NULL,
   PRIMARY KEY (`order_id`),
   FOREIGN KEY (`order_customer`) REFERENCES `customers`(`customer_id`),
+  UNIQUE (`order_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS  `market_order_items` (
   `orderItem_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `orderItem_order` INT UNSIGNED NOT NULL,
@@ -863,7 +871,7 @@ CREATE TABLE IF NOT EXISTS  `market_order_items` (
   `orderItem_disc` DECIMAL(6,2) NOT NULL,
   `orderItem_total` DECIMAL(12, 2) NOT NULL,
   PRIMARY KEY (`orderItem_id`),
-  KEY `orderItem_order` REFERENCES `market_orders`(`order_id`),
-  KEY `orderItem_product` REFERENCES `market_products`(`product_id`),
+  FOREIGN KEY (`orderItem_order`) REFERENCES `market_orders`(`order_id`),
+  FOREIGN KEY (`orderItem_product`) REFERENCES `market_products`(`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
